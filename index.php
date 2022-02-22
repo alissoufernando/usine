b4664980a38932b3b45684b80ef406875a40f94d
Alissou <alissounani@gmail.com>
2022-02-22 16:48:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <title>usine</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="index.css"/>
+    <link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.min.css"/>
+</head>
+<body>
+<header class="header">
+    <div class="menu">
+        <img src="images/logo.png">
+        <ul>
+            <li><a href="index.php">ACCEUIL</a></li>
+            <li><a href="propos.php">A PROPOS DE L'USINE</a></li>
+            <li><a href="#">PRODUITS</a></li>
+           <li><a href="#">CONTACT</a></li>
+        </ul>
+    </div>
+    <h1>C'EST<br> LA QUE <br>TOUT COMMENCE</h1>
+    <button>Découvrez notre technologie</button>
+    <img id="images" src="images/acceuil2.jpg">
+</header>
+<div class="bas">
+    <p>Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec<br>
+        sagittis sem nibh id elit. Duis sed odio sit amet<br>
+        nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.</p>
+ <img src="images/bas.png">
+</div>
+<section>
+    <div class="article1">
+        <div class="article2">
+            <h1>Nous créons<br>
+                une la technologie</h1>
+            <h2>Aenean sollicitudin, lorem quis bibendum auctor nisi<br>
+                elit consequat ipsum, nec sagittis</h2>
+            <div class="contenu">
+                Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a,
+                convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis.
+                Nulla imperdiet sit amet magna.
+                Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu.
+            </div>
+            <div class="contenu">
+                Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec
+                sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
+                Morbi accumsan ipsum velit.
+                Nam nec tellusin orci enim, luctus et interdum adipiscing wisi.
+            </div>
+            <button>Aller aux produits</button>
+        </div>
+    </div>
+    <div class="produit">
+        <div class="produit1">
+            <img src="images/home2.png">
+            <h4>Aenean sollicitudin lorem quis bibendum</h4>
+            <p>
+                Pellentesque a fermentum dui; sit amet dapibus nulla. Quisque vel hendrerit leo.
+                Donec euismod commodo dui at lobortis. Integer ornare pellentesque leo posuere.
+            </p>
+        </div>
+        <div class="produit1">
+            <img src="images/home1.png">
+            <h4>In quis placerat leo metus nullam efficitur est metus</h4>
+            <p>
+                Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis,
+                malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies.
+            </p>
+        </div>
+        <div class="produit1">
+            <img src="images/home3.png">
+            <h4>Praesent volutpat volutpat morbi gravida orci aliquam</h4>
+            <p>
+                Pellentesque a fermentum dui; sit amet dapibus nulla. Quisque vel hendrerit leo.
+                Donec euismod commodo dui at lobortis. Integer ornare pellentesque leo posuere.
+            </p>
+        </div>
+        <div class="produit1">
+            <img src="images/home.png">
+            <h4>Dapibus risus ante sodales lectus blandit eu tempor diam</h4>
+            <p>
+                Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna.
+                Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus.
+            </p>
+        </div>
+        <div class="produit1">
+            <img src="images/home1.png">
+            <h4>Aenean sollicitudin lorem quis bibendum</h4>
+            <p>
+                Pellentesque a fermentum dui; sit amet dapibus nulla. Quisque vel hendrerit leo.
+                Donec euismod commodo dui at lobortis. Integer ornare pellentesque leo posuere.
+            </p>
+        </div>
+    </div>
+    <div class="local">
+        <div class="local1">
+            <h1>
+                Nous sommes<br>
+                répartis dans<br>
+                le monde entier
+            </h1>
+        </div>
+        <div id="seul" class="local1">
+            <img src="images/home4.png">
+        </div>
+        <div class="local1">
+            <p>Sed consequat varius eros eget vestibulum! Nullam bibendum, eros quis aliquet dictum, tortor tortor lacinia metus,
+                ut euismod neque felis eget ipsum massa nunc. Vestibulum sem mauris, tristique vitae rhoncus id,
+                fringilla sit amet ligula
+            </p>
+            <a href="#">Aller au centre de contact</a>
+        </div>
+    </div>
+    <div class="fabrication">
+        <div class="plus">
+            <button>En savoir plus sur nous</button>
+        </div>
+        <div class="left">
+            <h1>Nous fabriquons<br>
+                des éléments de voiture depuis <br>1935</h1>
+            <p>Aenean sollicitudin, lorem quis bibendum auctor nisi elit<br>
+                consequat ipsum, nec sagittis.</p>
+            <div class="fabrication1">
+
+                Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.
+                Duis sed odio sit amet nibh vulputate
+                cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec
+                tellusin orci enim, luctus et interdum adipiscing wisi.
+            </div>
+            <div class="fabrication1">
+                Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum
+                commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum
+                in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna.
+                Vestibulum dapibus, mauris nec malesuada fames ac.
+            </div>
+        </div>
+    </div>
+    <div class="propos">
+        <div class="propos1">
+            <h3>Quelques liens</h3>
+            <ul>
+                <li> &raquo Quent per conubia nostra</li>
+                <li>&raquo Nceptos himenaeos</li>
+                <li>&raquo Mauris in erat</li>
+                <li>&raquo Lustoac</li>
+            </ul>
+        </div>
+        <div class="propos1">
+            <h3>Nos offres</h3>
+            <ul>
+                <li>&raquo Aenean sollicitudin orem qui</li>
+                <li>&raquo Duis sed odio sit amet nibh</li>
+                <li>&raquo Morbi accumsan ipsum velit</li>
+                <li>&raquo Nam nec tellus</li>
+                <li>&raquo Class aptent taciti sociosqu</li>
+                <li>&raquo Mauris in erat justo</li>
+                <li>&raquo Sed non neque</li>
+            </ul>
+        </div>
+        <div class="propos1">
+            <h3>À propos de nous</h3>
+            <ul>
+                <li>&raquo Duis sed odio sit amet nibh</li>
+                <li>&raquo Morbi accumsan ipsum velit</li>
+                <li>&raquo Nam nec tellus</li>
+                <li>&raquo Class aptent taciti sociosqu</li>
+                <li>&raquo Mauris in erat justo</li>
+                <li>&raquo Sed non neque</li>
+            </ul>
+        </div>
+        <div class="propos1">
+            <h3>Départements</h3>
+            <ul>
+                <li>&raquo Nam nec tellus</li>
+                <li>&raquo Class aptent taciti sociosqu</li>
+                <li>&raquo Mauris in erat justo</li>
+                <li>&raquo Sed non neque</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<footer class="pieds">
+    <p>© 2018 EIDA | Assurance <a href="#">EIDA CONSULTING</a></p>
+</footer>
+</body>
+</html>
---
+++ propos.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <title> A propos de l'usine</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="propos.css"/>
+    <link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.min.css"/>
+</head>
+<body>
+<header class="header">
+    <div class="menu">
+        <img src="images/logo.png">
+        <ul>
+            <li><a href="index.php">ACCEUIL</a></li>
+            <li><a href="propos.php">A PROPOS DE L'USINE</a></li>
+            <li><a href="#">PRODUITS</a></li>
+            <li><a href="#">CONTACT</a></li>
+        </ul>
+    </div>
+    <h1>A propos de l'usine</h1>
+</header>
+<section>
+    <div class="tete">
+        <img src="images/home8.png">
+        <h3>Le confort et la fiabilité de nos éléments ne se racontent pas, vous apprécierez <br>vous-même.</h3>
+    </div>
+    <div class="histoire">
+        <div class="article" id="seul">
+            <h2>Comment c'était au<br>
+                début?</h2>
+            <h4>Sed id nisi lectus! Fusce ac magna gravida, tincidunt ipsum ac<br>
+                malesuada ante! Nam vel facilisis neque.</h4>
+            <div class="article1">
+                <img src="images/home9.jpg">
+                Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat,
+                velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.
+                Telis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi.
+                Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat velit.
+            </div>
+            <div class="article1">
+                Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.
+                Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.<br>
+
+                <br>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
+                Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per
+                conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit
+                amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat
+                sed fermentum feugiat, velit mauris egestas quam, ut aliquam.
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <div class="article">
+            <h1>Nous proposons des solutions simples
+                pour vous simplifier la vie</h1>
+            <h4>Sed id nisi lectus! Fusce ac magna gravida, tincidunt ipsum ac<br>
+                malesuada ante! Nam vel facilisis neque.</h4>
+            <div class="article1">
+                Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.
+                Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio
+                tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit.
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo.<br>
+
+                <br>Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi.
+                Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam,
+                ut aliquam massa nisl quis neque.
+
+            </div>
+            <div class="article1">
+                Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit.
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo.
+                Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin
+                condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam
+                massa nisl quis neque. Suspendisse in orci enim.
+            </div>
+        </div>
+    </div>
+    <div class="partenaire">
+        <h1>Devenir notre partenaire</h1>
+        <p>
+            Psum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a<br> sit amet mauris. Morbi
+            accumsan ipsum velit.
+        </p>
+        <button>Aller au contacts</button>
+    </div>
+    <div class="propos">
+        <div class="propos1">
+            <h3>Quelques liens</h3>
+            <ul>
+                <li> &raquo Quent per conubia nostra</li>
+                <li>&raquo Nceptos himenaeos</li>
+                <li>&raquo Mauris in erat</li>
+                <li>&raquo Lustoac</li>
+            </ul>
+        </div>
+        <div class="propos1">
+            <h3>Nos offres</h3>
+            <ul>
+                <li>&raquo Aenean sollicitudin orem qui</li>
+                <li>&raquo Duis sed odio sit amet nibh</li>
+                <li>&raquo Morbi accumsan ipsum velit</li>
+                <li>&raquo Nam nec tellus</li>
+                <li>&raquo Class aptent taciti sociosqu</li>
+                <li>&raquo Mauris in erat justo</li>
+                <li>&raquo Sed non neque</li>
+            </ul>
+        </div>
+        <div class="propos1">
+            <h3>À propos de nous</h3>
+            <ul>
+                <li>&raquo Duis sed odio sit amet nibh</li>
+                <li>&raquo Morbi accumsan ipsum velit</li>
+                <li>&raquo Nam nec tellus</li>
+                <li>&raquo Class aptent taciti sociosqu</li>
+                <li>&raquo Mauris in erat justo</li>
+                <li>&raquo Sed non neque</li>
+            </ul>
+        </div>
+        <div class="propos1">
+            <h3>Départements</h3>
+            <ul>
+                <li>&raquo Nam nec tellus</li>
+                <li>&raquo Class aptent taciti sociosqu</li>
+                <li>&raquo Mauris in erat justo</li>
+                <li>&raquo Sed non neque</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<footer class="pieds">
+    <p>© 2018 EIDA | Assurance <a href="#">EIDA CONSULTING</a></p>
+</footer>
+</body>
+</html>
